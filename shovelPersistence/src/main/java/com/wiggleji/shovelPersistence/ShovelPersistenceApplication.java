abb639e991e0fc5d8c89224bd79935d0b5b3c715
wiggleji <roamgom@gmail.com>
2024-06-20 22:30:30 +0900

    init shovelPersistence

---
+++ shovelPersistence/src/main/java/com/wiggleji/shovelPersistence/ShovelPersistenceApplication.java
@@ -0,0 +1,13 @@
+package com.wiggleji.shovelPersistence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShovelPersistenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShovelPersistenceApplication.class, args);
+	}
+
+}
---
+++ shovelPersistence/src/test/java/com/wiggleji/shovelPersistence/ShovelPersistenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wiggleji.shovelPersistence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShovelPersistenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
